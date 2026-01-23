95e99f37047e48f7dad9cc591d6097ae491f3651
소희 <y04828@naver.com>
2026-01-23 18:48:50 +0900

    [feat] 알림푸쉬설정 controller 구현

--- src/main/java/com/example/afternote/domain/user/controller/UserController.java
+++ src/main/java/com/example/afternote/domain/user/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.afternote.domain.user.controller;
 
+import com.example.afternote.domain.user.dto.UserPushSettingResponse;
 import com.example.afternote.domain.user.dto.UserResponse;
 import com.example.afternote.domain.user.dto.UserUpdateProfileRequest;
+import com.example.afternote.domain.user.dto.UserUpdatePushSettingRequest;
 import com.example.afternote.domain.user.service.UserService;
 import com.example.afternote.global.common.ApiResponse;
 import com.example.afternote.global.resolver.UserId;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User API", description = "회원 관련 API")
@@ -44,6 +44,33 @@ public class UserController {
     ) {
         return ApiResponse.success(
                 userService.updateMyProfile(userId, request)
+        );
+    }
+
+    @Operation(
+            summary = "푸시 알림 설정 조회 API",
+            description = "로그인한 사용자의 푸시 알림 수신 설정을 불러옵니다."
+    )
+    @GetMapping("/push-settings")
+    public ApiResponse<UserPushSettingResponse> getMyPushSettings(
+            @UserId Long userId
+    ) {
+        return ApiResponse.success(
+                userService.getMyPushSettings(userId)
+        );
+    }
+
+    @Operation(
+            summary = "푸시 알림 설정 수정 API",
+            description = "로그인한 사용자의 푸시 알림 수신 설정을 수정합니다."
+    )
+    @PatchMapping("/push-settings")
+    public ApiResponse<UserPushSettingResponse> updateMyPushSettings(
+            @UserId Long userId,
+            @RequestBody UserUpdatePushSettingRequest request
+    ) {
+        return ApiResponse.success(
+                userService.updateMyPushSettings(userId, request)
         );
     }
 }
